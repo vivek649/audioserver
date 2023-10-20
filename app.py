823c032a549edb02137f5a9232f4cfe56a1f133c
vivek649 <78976350+vivek649@users.noreply.github.com>
2023-10-20 22:23:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,31 +5,33 @@ from io import BytesIO
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "my_secret_key"
 
-@app.route("/video/<video_id>", methods=["GET"])
-def video(video_id):
-    youtube_link = f"https://www.youtube.com/watch?v={video_id}"
-    buffer = BytesIO()
-    url = YouTube(youtube_link)
-    
-    # Select the stream with 1080p resolution (if available)
-    video_stream = url.streams.filter(res="1080p", file_extension="mp4").first()
+@app.route("/", methods=["POST", "GET"])
+def index():
+    if request.method == "POST":
+        session["link"] = request.form.get("url")
+        url = YouTube(session["link"])
+        url.check_availability()
+        return render_template("download.html", url=url)
 
-    if video_stream:
-        video_stream.stream_to_buffer(buffer)
+    return render_template('index.html')
+
+@app.route("/api", methods=["GET"])
+def download():
+    youtube_link = request.args.get("url")
+    if youtube_link:
+        buffer = BytesIO()
+        url = YouTube(youtube_link)
+        video = url.streams.get_highest_resolution()
+
+        # Stream the video to the buffer
+        video.stream_to_buffer(buffer)
         buffer.seek(0)
 
-        video_path = f"{url.title}.mp4"
-
-        # Fetch and set the thumbnail image
-        thumbnail_url = url.thumbnail_url
-        thumbnail_data = BytesIO(YouTube(thumbnail_url).thumbnail_url)
-        
-        with open(video_path, "wb") as video_file:
-            video_file.write(buffer.read())
-
-        return send_file(video_path, as_attachment=True)
+        return send_file(buffer, as_attachment=True, download_name=video.title, mimetype=video.mime_type)
 
     return "Invalid or missing YouTube link parameter."
 
 if __name__ == '__main__':
     app.run(debug=True)
+    # Happy Coding :-)
+
